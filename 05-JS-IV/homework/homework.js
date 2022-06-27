77155833bb652cf7c12cd453c41055c34891deb5
ClaPeralta <claudiomartinperalta@gmail.com>
2022-06-27 20:10:01 -0300

    corregido el 3

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -31,9 +31,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-  let objeto = {
-    metodo: function () {},
-  };
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
